ebfdedebdb5133186d29ce918324ec86df17f07a
RusirikaFernando <thathsaranif7@gmail.com>
2023-06-12 10:26:40 +0530

    gashjdchj bg run add my UIs - rusirika updated i have added guitar and violin pages

---
+++ app/src/main/java/com/example/myapplication/guitarChords.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class guitarChords extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guitar_chords);
+    }
+
+    public void back(View v){
+        Intent i= new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void close(View v){
+        Intent i= new Intent(this, rabanaHome.class);
+        startActivity(i);
+    }
+}
