116327d21ca7c8bc4718cf4d1dbded16d0ad48a8
RusirikaFernando <thathsaranif7@gmail.com>
2023-06-12 14:24:25 +0530

    gashjdchj bg run add my UIs - rusirika updated i have added guitar and violin pages updated updated again hiiiiii

--- app/src/main/java/com/example/myapplication/searchBar.java
+++ app/src/main/java/com/example/myapplication/searchBar.java
@@ -7,14 +7,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 
 public class searchBar extends AppCompatActivity {
+private Button openKeyboardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_bar);
+
+        openKeyboardButton = findViewById(R.id.openkeyboard);
+        openKeyboardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openKeyboard();
+            }
+        });
     }
+    private void openKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+        }
+    }
+
+
     public void search(View v){
         //show the soft keyboard
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -24,7 +42,17 @@ public class searchBar extends AppCompatActivity {
         Intent i= new Intent(this, rabanaHome.class);
         startActivity(i);
     }
-    public void next(View n){
+
+    public void gnext(View n){
+        Intent i = new Intent(this, guitarChords.class);
+        startActivity(i);
+    }
+
+    public void vnext(View n){
+        Intent i = new Intent(this, violinChords.class);
+        startActivity(i);
+    }
+    public void pnext(View n){
         Intent i = new Intent(this, pianoChords.class);
         startActivity(i);
     }
